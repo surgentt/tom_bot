5b8eed3fe91acc838659688c2014b17e965897b5
Thomas Surgent <surgentt@gmail.com>
2018-12-12 11:26:01 -0500

    Can request differences in weather from prior day

--- app/tom_bot.rb
+++ app/tom_bot.rb
@@ -1,33 +1,49 @@
 require_relative '../config/environment'
 
 class TomBot
-  attr_reader :client
+  attr_reader :client, :default_channel
 
   def initialize
     @running = true
     @client = Slack::RealTime::Client.new
+    @default_channel = 'GEQ1ZN24W'
     listen
+    # large_weather_change
   end
 
   def listen
     client.on :hello do
       puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
     end
-
     client.on :message do |data|
-      case data.message.text
-      when '<@UEPM3KUTH> hi bot' then
-        slack_message(data.channel, "Hi <@#{data.message.user}>!")
-      when '<@UEPM3KUTH> Weather now' then 
-        slack_message(data.channel, current_weather_msg)
-      when '<@UEPM3KUTH> Weather tomorrow' then 
-        slack_message(data.channel, weather_tomorrow_msg)
+      if data.message
+        case data.message.text
+        when '<@UEPM3KUTH> hi bot' then
+          slack_message(data.channel, "Hi <@#{data.message.user}>!")
+        when '<@UEPM3KUTH> Weather now' then 
+          slack_message(data.channel, current_weather_msg)
+        when '<@UEPM3KUTH> Weather tomorrow' then 
+          slack_message(data.channel, weather_tomorrow_msg)
+        when '<@UEPM3KUTH> Weather difference' then 
+          slack_message(data.channel, large_weather_change)
+        else
+          slack_message(data.channel, "Sorry <@#{data.user}>, what?")
+        end
       else
-        slack_message(data.channel, "Sorry <@#{data.user}>, what?")
+        puts data
       end
     end
-
     client.start!
+  end
+
+  # TODO: Run rake task at 8am ever morning.
+  def large_weather_change
+    current_forecast   = ForecastIO.forecast(40.7128, -74.0060)['currently']
+    yesterday_forecast = ForecastIO.forecast(40.7128, -74.0060,  time: (Time.now-1.day).to_i)['currently']
+    temp_change = (current_forecast['temperature'] - yesterday_forecast['temperature']).abs
+    if temp_change > 8
+      slack_message(self.default_channel, "Temperature Change from Yesterday of #{temp_change.to_s}")
+    end
   end
 
   private
